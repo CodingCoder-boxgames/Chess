8ffd79d58f3c4499e0ee1b9ce91ed97012f4631c
Coding Coder <staf.van.acker04@gmail.com>
2023-01-21 19:32:34 +0100

    new commit

--- src/piece.py
+++ src/piece.py
@@ -7,6 +7,8 @@ class Piece:
         self.color = color
         value_sign = 1 if color == "white" else -1
         self.value = value * value_sign
+        self.moves = []
+        self.moved = False
         self.texture = texture
         self.set_texture()
         self.texture_rect = texture_rect
@@ -15,6 +17,8 @@ class Piece:
         self.texture = os.path.join(
             f'assets/images/imgs-{size}px/{self.color}_{self.name}.png'
         )
+    def add_moves(self, move):
+        self.moves.append(move)
     
 class Pawn(Piece):
     def __init__(self, color):
